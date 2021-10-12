eb4e92d2fe63e9ec62ef8c42cc0dc588f2b76579
osho <oshosingh25@gmail.com>
2021-10-12 18:24:58 +0530

    first commit

---
+++ src/main/java/com/backend/controller/BackendController.java
@@ -0,0 +1,13 @@
+package com.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BackendController {
+
+	@GetMapping("/say/hello")
+	public String sayHello() {
+		return "Hello world";
+	}
+}
