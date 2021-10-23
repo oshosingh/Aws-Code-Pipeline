9335c7f28c03ba81a50848f88dc8f4d669f84c2f
osho singh <oshosingh25@gmail.com>
2021-10-23 14:36:40 +0530

    added env variable

--- deploy/codedeploy.sh
+++ deploy/codedeploy.sh
@@ -1,7 +1,7 @@
 ecrname="default"
 cd /home/ubuntu/app/backend
 docker-compose rm -sf deployment || true
-docker rmi 131872871854.dkr.ecr.us-east-2.amazonaws.com/deployment:latest
-aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 131872871854.dkr.ecr.us-east-2.amazonaws.com
-docker pull 131872871854.dkr.ecr.us-east-2.amazonaws.com/deployment:latest
+docker rmi ${repoimage}
+aws ecr get-login-password --region ${region} | docker login --username ${username} --password-stdin ${password}
+docker pull ${repoimage}
 docker-compose up -d deployment
