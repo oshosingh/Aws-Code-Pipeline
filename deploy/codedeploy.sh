02bd26fdd902a4c855ee3d27f0140ff35920a517
osho <oshosingh25@gmail.com>
2021-12-29 20:29:52 +0530

    initial commit

--- deploy/codedeploy.sh
+++
@@ -1,7 +0,0 @@
-ecrname="default"
-cd /home/ubuntu/app/backend
-docker-compose rm -sf deployment || true
-docker rmi ${repoimage}
-aws ecr get-login-password --region ${region} | docker login --username ${username} --password-stdin ${password}
-docker pull ${repoimage}
-docker-compose up -d deployment
