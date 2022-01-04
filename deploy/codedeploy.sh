8aca79bfd2416b885d79c213238b6aa75e1fc0a6
osho singh <oshosingh25@gmail.com>
2022-01-04 12:36:57 +0530

    Updated file

--- deploy/codedeploy.sh
+++ deploy/codedeploy.sh
@@ -1,6 +1,6 @@
 cd /home/ubuntu/app/backend
 docker-compose rm -sf deployment || true
 docker rmi 131872871854.dkr.ecr.amazonaws.com/springbackend:latest
-aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 131872871854.dkr.ecr.amazonaws.com/springbackend
+aws ecr get-login-password --region <region> | docker login --username AWS --password-stdin 131872871854.dkr.ecr.amazonaws.com/springbackend
 docker pull 131872871854.dkr.ecr.amazonaws.com/springbackend:latest
 docker-compose up -d deployment
